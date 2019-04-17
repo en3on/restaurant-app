6ab2e777b86b072e2b5d1cdbfb1ebb7dafa916e4
Adam Ladell <adamladellsing@gmail.com>
2019-04-17 14:47:13 +1000

    created model and controller for Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,35 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    @restaurant.save ? ( redirect_to @restaurant ):( render 'new' )
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    @restaurant.update(restaurant_params) ? ( redirect_to @restaurant ) : ( render 'edit' )
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.permit(:title, :address, :description, :food_type)
   end
 end
